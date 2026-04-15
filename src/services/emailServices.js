8709527e1bd6f3cfdf6e901aecc8295938faae9f
Nayan Jyoti <nayan2024cswork@gmail.com>
2026-04-15 23:36:09 +0530

    change ipv6 to ipv4 forcefully hardcoded

--- src/services/emailServices.js
+++ src/services/emailServices.js
@@ -1,14 +1,9 @@
 import nodemailer from "nodemailer";
-import dns from "dns";
-
-// ✅ Force IPv4
-dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "142.250.152.108", // ✅ Gmail SMTP IPv4 (hardcoded)
   port: 465,
   secure: true,
-  family: 4, // ✅ force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -30,3 +25,4 @@ export const sendEmailOTP = async (email, otp) => {
     throw error;
   }
 };
+
